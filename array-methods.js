054463eb436fd1d41a589184a1fb56569354e20a
feisal <adanfeisal18@gmaile.com>
2022-10-23 15:32:12 +0300

    have updated  Challenge 1: Use .forEach() of array methods.js file

--- array-methods.js
+++ array-methods.js
@@ -58,11 +58,20 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = [];
+
 // console.log(fullName);
-full_Name = runners.forEach(function(item){
-  return console.log(`item.first_name + "last_name"`);
-});
+
+const fullNames = [];
+  const runner = [
+   {first_name:"Charmain",last_name:"Seiler"}
+  ]
+  function runnersNames({first_name, last_name}){
+    return `first_name: ${first_name}, last_name: ${last_name}`
+  }
+  runners.forEach((element)=>{
+    fullNames.push(runnersNames(element));
+  })
+  console.log(fullNames);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
